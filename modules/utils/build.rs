c130ecbce863ed5229e942b546fc01c0aaf508db
Maxwell Flitton <maxwellflitton@gmail.com>
2023-10-31 11:18:30 +0000

    adding the no verfiy for the ONNX build (#3)
    * adding the no verfiy for the ONNX build
    
    * adding the no verfiy for the ONNX build

--- modules/utils/build.rs
+++ modules/utils/build.rs
@@ -15,6 +15,13 @@ fn main() {
         .status()
         .expect("failed to execute process");
 
+    // move the onnx lib to the right place
+    // let _ = Command::new("sh")
+    //     .arg("-c")
+    //     .arg("cd onnx_driver && cargo build")
+    //     .status()
+    //     .expect("failed to execute process");
+
     // let _ = Command::new("sh")
     //     .arg("-c")
     //     .arg("cd ../onnx_driver && cargo build")
