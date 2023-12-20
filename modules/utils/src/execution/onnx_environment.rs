b5809e6792628ff193c35174bcb48053eb9be9cb
Maxwell Flitton <maxwellflitton@gmail.com>
2023-12-20 13:13:15 +0000

    flag compilations for ONNX runtime (#5)
    * flag compilations for ONNX runtime
    
    * removing main.rs
    
    * removing the verbose arguement
    
    * inspecting github actions
    
    * inspecting the build
    
    * removing the delete in the environment
    
    * adding writes depending on OS
    
    * removing the ORT_DYLIB_PATH env var after the load
    
    * adding writes depending on OS
    
    * altering binary builds
    
    * reinserting test pipeline
    
    * documentation added and ready for deployment

--- modules/utils/src/execution/onnx_environment.rs
+++ modules/utils/src/execution/onnx_environment.rs
@@ -5,31 +5,55 @@ use std::sync::Arc;
 
 
 // Compiles the ONNX module into the rust binary.
-#[cfg(all(target_os = "macos", not(doc)))]
+#[cfg(all(target_os = "macos", not(doc), not(onnx_runtime_env_var_set)))]
 pub static LIB_BYTES: &'static [u8] = include_bytes!("../../onnx_driver/target/debug/libonnxruntime.dylib");
 
-#[cfg(all(any(target_os = "linux", target_os = "android"), not(doc)))]
+#[cfg(all(any(target_os = "linux", target_os = "android"), not(doc), not(onnx_runtime_env_var_set)))]
 pub static LIB_BYTES: &'static [u8] = include_bytes!("../../onnx_driver/target/debug/libonnxruntime.so");
 
-#[cfg(all(target_os = "windows", not(doc)))]
-pub static LIB_BYTES: &'static [u8] = include_bytes!("../../onnx_driver/target/debug/libonnxruntime.dll");
+#[cfg(all(target_os = "windows", not(doc), not(onnx_runtime_env_var_set)))]
+pub static LIB_BYTES: &'static [u8] = include_bytes!("../../onnx_driver/target/debug/onnxruntime.dll");
 
 // Fallback for documentation and other targets
-#[cfg(any(doc, not(any(target_os = "macos", target_os = "linux", target_os = "android", target_os = "windows"))))]
+#[cfg(any(doc, onnx_runtime_env_var_set, not(any(target_os = "macos", target_os = "linux", target_os = "android", target_os = "windows"))))]
 pub static LIB_BYTES: &'static [u8] = &[];
 
 
 // the ONNX environment which loads the library
 pub static ENVIRONMENT: Lazy<Arc<Environment>> = Lazy::new(|| {
-    let _ = std::fs::write("./libonnxruntime.dylib", LIB_BYTES);
-    std::env::set_var("ORT_DYLIB_PATH", "./libonnxruntime.dylib");
-    let environment = Arc::new(
-        Environment::builder()
-            .with_execution_providers([ExecutionProvider::CPU(Default::default())])
-            .build().unwrap()
-    );
-    let _ = std::fs::remove_file("./libonnxruntime.dylib");
-    environment
+
+    // if the "ONNXRUNTIME_LIB_PATH" is provided we do not need to compile the ONNX library, instead we just point to the library
+    // in the "ONNXRUNTIME_LIB_PATH" and load that.
+    match std::env::var("ONNXRUNTIME_LIB_PATH") {
+        Ok(path) => {
+            std::env::set_var("ORT_DYLIB_PATH", path);
+            return Arc::new(
+                Environment::builder()
+                    .with_execution_providers([ExecutionProvider::CPU(Default::default())])
+                    .build().unwrap()
+            );
+        },
+        // if the "ONNXRUNTIME_LIB_PATH" is not provided we use the `LIB_BYTES` that is the ONNX library compiled into the binary.
+        // we write the `LIB_BYTES` to a temporary file and then load that file.
+        Err(_) => {
+
+            #[cfg(any(not(doc), not(onnx_runtime_env_var_set)))]
+            let _ = std::fs::write("./libonnxruntime.dylib", LIB_BYTES);
+            
+            std::env::set_var("ORT_DYLIB_PATH", "./libonnxruntime.dylib");
+            let environment = Arc::new(
+                Environment::builder()
+                    .with_execution_providers([ExecutionProvider::CPU(Default::default())])
+                    .build().unwrap()
+            );
+            std::env::remove_var("ORT_DYLIB_PATH");
+
+            #[cfg(any(not(doc), not(onnx_runtime_env_var_set)))]
+            let _ = std::fs::remove_file("./libonnxruntime.dylib");
+            
+            return environment
+        }
+    }
 });
 
 
