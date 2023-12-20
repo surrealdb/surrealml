bf652ea59c570b798922c532e63282c99d57d8d0
Maxwell Flitton <maxwellflitton@gmail.com>
2023-12-20 13:13:15 +0000

    flag compilations for ONNX runtime (#5)
    * flag compilations for ONNX runtime
    
    * removing main.rs
    
    * removing the verbose arguement
    
    * inspecting github actions
    
    * inspecting the build
    
    * removing the delete in the environment
    
    * adding writes depending on OS
    
    * removing the ORT_DYLIB_PATH env var after the load
    
    * adding writes depending on OS
    
    * altering binary builds
    
    * reinserting test pipeline
    
    * documentation added and ready for deployment

--- modules/utils/build.rs
+++ modules/utils/build.rs
@@ -2,23 +2,55 @@ use std::process::Command;
 
 fn main() {
 
-    let _ = Command::new("sh")
-        .arg("-c")
-        .arg("cargo new onnx_driver && cd onnx_driver && echo 'ort = \"1.16.2\"' >> Cargo.toml
-        ")
-        .status()
-        .expect("failed to execute process");
+    match std::env::var("ONNXRUNTIME_LIB_PATH") {
+        Ok(_) => {
+            println!("cargo:rustc-cfg=onnx_runtime_env_var_set");
+        },
+        Err(_) => {
+            #[cfg(not(windows))]
+            {
+                let _ = Command::new("sh")
+                .arg("-c")
+                .arg("cargo new onnx_driver && cd onnx_driver && echo 'ort = \"1.16.2\"' >> Cargo.toml
+                ")
+                .status()
+                .expect("failed to execute process");
+            }
 
-    let _ = Command::new("sh")
-        .arg("-c")
-        .arg("cd onnx_driver && cargo build")
-        .status()
-        .expect("failed to execute process");
+            #[cfg(windows)]
+            {
+                // let _ = Command::new("cmd")
+                // .args(&["/C", "cargo new onnx_driver && cd onnx_driver && echo ort = \"1.16.2\" >> Cargo.toml"])
+                // .status()
+                // .expect("failed to execute process");
+                let _ = Command::new("powershell")
+                    .arg("-Command")
+                    .arg("cargo new onnx_driver; Set-Location onnx_driver; Add-Content -Path .\\Cargo.toml -Value 'ort = \"1.16.2\"'")
+                    .status()
+                    .expect("failed to execute process");
+            }
 
-    // let _ = Command::new("sh")
-    //     .arg("-c")
-    //     .arg("cd ../onnx_driver && cargo build")
-    //     .status()
-    //     .expect("failed to execute process");
+            #[cfg(not(windows))]
+            {
+                let _ = Command::new("sh")
+                    .arg("-c")
+                    .arg("cd onnx_driver && cargo build")
+                    .status()
+                    .expect("failed to execute process");
+            }
 
+            #[cfg(windows)]
+            {
+                let _ = Command::new("cmd")
+                    .args(&["/C", "cd onnx_driver && cargo build"])
+                    .status()
+                    .expect("failed to execute process");
+            }
+        }
+    }
 }
+
+// fn main() {
+//     // println!("cargo:rustc-cfg=onnx_runtime_env_var_set");
+//     println!("test");
+// }
