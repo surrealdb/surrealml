07cc24ec66e58dd91b41ee3506599b4bb3874de4
Maxwell Flitton <maxwellflitton@gmail.com>
2024-01-05 15:05:06 +0000

    now tracking the build (#11)
    Here we track the `target` directory so it does not matter where the target is, the core will still be able to build no matter where the target is

--- modules/utils/build.rs
+++ modules/utils/build.rs
@@ -5,6 +5,13 @@ use std::fs;
 
 fn main() -> std::io::Result<()> {
 
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
+    let out_path = Path::new(&out_dir);
+    let build_dir = out_path
+        .ancestors()  // This gives an iterator over all ancestors of the path
+        .nth(3)       // 'nth(3)' gets the fourth ancestor (counting from 0), which should be the debug directory
+        .expect("Failed to find debug directory");
+
     match std::env::var("ONNXRUNTIME_LIB_PATH") {
         Ok(_) => {
             println!("cargo:rustc-cfg=onnx_runtime_env_var_set");
@@ -17,12 +24,10 @@ fn main() -> std::io::Result<()> {
                 // ref s if s.contains("android") => "android", => not building for android
                 _ => panic!("Unsupported target os")
             };
-            let profile = match env::var("PROFILE").unwrap() {
-                ref s if s.contains("release") => "release",
-                ref s if s.contains("debug") => "debug",
-                _ => panic!("Unsupported profile")
-            };
-            let lib_path = Path::new("target").join(profile).join(target_lib);
+
+            let lib_path = build_dir.join(target_lib);
+            let lib_path = lib_path.to_str().unwrap();
+
             // put it next to the file of the embedding
             let destination = Path::new(target_lib);
             fs::copy(lib_path, destination)?;
