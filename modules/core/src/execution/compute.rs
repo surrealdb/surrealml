e5e99ff42aa8a82aa26b9bbeb40495d31c885722
Maxwell Flitton <maxwellflitton@gmail.com>
2024-01-26 11:54:07 +0000

    Python engine adapters (#19)
    * updating interfaces for different packages
    
    * updating testing suite so surml loading and execution can be tested in python unit tests
    
    * adding test model build pipeline and unit tests
    
    * sklearn linear now tested and passing for buffered compute and raw compute

--- modules/core/src/execution/compute.rs
+++ modules/core/src/execution/compute.rs
@@ -2,7 +2,7 @@
 use crate::storage::surml_file::SurMlFile;
 use std::collections::HashMap;
 use ndarray::{ArrayD, CowArray};
-use ort::{SessionBuilder, Value};
+use ort::{SessionBuilder, Value, session::Input};
 use super::onnx_environment::ENVIRONMENT;
 
 
@@ -29,6 +29,24 @@ impl <'a>ModelComputation<'a> {
         ndarray::arr1::<f32>(&buffer).into_dyn()
     }
 
+    /// Creates a vector of dimensions for the input tensor from the loaded model.
+    /// 
+    /// # Arguments
+    /// * `input_dims` - The input dimensions from the loaded model.
+    /// 
+    /// # Returns
+    /// A vector of dimensions for the input tensor to be reshaped into from the loaded model.
+    fn process_input_dims(input_dims: &Input) -> Vec<usize> {
+        let mut buffer = Vec::new();
+        for dim in input_dims.dimensions() {
+            match dim {
+                Some(dim) => buffer.push(dim as usize),
+                None => buffer.push(1)
+            }
+        }
+        buffer
+    }
+
     /// Creates a Vector that can be used manipulated with other operations such as normalisation from a hashmap of keys and values.
     /// 
     /// # Arguments
@@ -39,7 +57,10 @@ impl <'a>ModelComputation<'a> {
     pub fn input_vector_from_key_bindings(&self, mut input_values: HashMap<String, f32>) -> Vec<f32> {
         let mut buffer = Vec::with_capacity(self.surml_file.header.keys.store.len());
         for key in &self.surml_file.header.keys.store {
-            let value = input_values.get_mut(key).unwrap();
+            let value = match input_values.get_mut(key) {
+                Some(value) => value,
+                None => panic!("Key {} not found in input values", key)
+            };
             buffer.push(std::mem::take(value));
         }
         buffer
@@ -52,28 +73,14 @@ impl <'a>ModelComputation<'a> {
     /// 
     /// # Returns
     /// The computed output tensor from the loaded model.
-    pub fn raw_compute(&self, tensor: ArrayD<f32>, dims: Option<(i32, i32)>) -> Result<Vec<f32>, String> {
-
-        let tensor_placeholder: ArrayD<f32>;
-        if dims.is_some() {
-            let dims = dims.unwrap();
-            let tensor = tensor.into_shape((dims.0 as usize, dims.1 as usize)).unwrap();
-            tensor_placeholder = tensor.into_dyn();
-        }
-        else {
-            tensor_placeholder = tensor;
-        }
-
-        // let environment = Arc::new(
-        //     Environment::builder()
-        //         .with_execution_providers([ExecutionProvider::CPU(Default::default())])
-        //         .build()
-        //         .map_err(|e| e.to_string())?
-        // );
+    pub fn raw_compute(&self, tensor: ArrayD<f32>, _dims: Option<(i32, i32)>) -> Result<Vec<f32>, String> {
         let session = SessionBuilder::new(&ENVIRONMENT).map_err(|e| e.to_string())?
                                                        .with_model_from_memory(&self.surml_file.model)
                                                        .map_err(|e| e.to_string())?;
-        let x = CowArray::from(tensor_placeholder);
+        let unwrapped_dims = ModelComputation::process_input_dims(&session.inputs[0]);
+        let tensor = tensor.into_shape(unwrapped_dims).map_err(|e| e.to_string())?;
+
+        let x = CowArray::from(tensor).into_dyn();
         let outputs = session.run(vec![Value::from_array(session.allocator(), &x).unwrap()]).map_err(|e| e.to_string())?;
 
         let mut buffer: Vec<f32> = Vec::new();
@@ -130,7 +137,10 @@ impl <'a>ModelComputation<'a> {
         }
 
         // apply the normaliser to the output
-        let output_normaliser = self.surml_file.header.output.normaliser.as_ref().unwrap();
+        let output_normaliser = match self.surml_file.header.output.normaliser.as_ref() {
+            Some(normaliser) => normaliser,
+            None => return Err(String::from("No normaliser present for output which shouldn't happen as passed initial check for"))
+        };
         let mut buffer = Vec::with_capacity(output.len());
 
         for value in output {
@@ -148,9 +158,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_raw_compute() {
-
-        let mut file = SurMlFile::from_file("./stash/test.surml").unwrap();
+    fn test_raw_compute_linear_sklearn() {
+        let mut file = SurMlFile::from_file("./model_stash/sklearn/surml/linear.surml").unwrap();
         let model_computation = ModelComputation {
             surml_file: &mut file,
         };
@@ -159,15 +168,17 @@ mod tests {
         input_values.insert(String::from("squarefoot"), 1000.0);
         input_values.insert(String::from("num_floors"), 2.0);
 
-        let output = model_computation.raw_compute(model_computation.input_tensor_from_key_bindings(input_values), None).unwrap();
+        let raw_input = model_computation.input_tensor_from_key_bindings(input_values);
+
+        let output = model_computation.raw_compute(raw_input, Some((1, 2))).unwrap();
         assert_eq!(output.len(), 1);
-        assert_eq!(output[0], 725.42053);
+        assert_eq!(output[0], 985.57745);
     }
 
 
     #[test]
-    fn test_buffered_compute() {
-        let mut file = SurMlFile::from_file("./stash/test.surml").unwrap();
+    fn test_buffered_compute_linear_sklearn() {
+        let mut file = SurMlFile::from_file("./model_stash/sklearn/surml/linear.surml").unwrap();
         let model_computation = ModelComputation {
             surml_file: &mut file,
         };
@@ -178,22 +189,37 @@ mod tests {
 
         let output = model_computation.buffered_compute(&mut input_values).unwrap();
         assert_eq!(output.len(), 1);
-        assert_eq!(output[0], 725.42053);
     }
 
     #[test]
-    fn test_raw_compute_trees() {
-            let mut file = SurMlFile::from_file("./stash/forrest.surml").unwrap();
-            let model_computation = ModelComputation {
-                surml_file: &mut file,
-            };
+    fn test_raw_compute_linear_torch() {
+        let mut file = SurMlFile::from_file("./model_stash/torch/surml/linear.surml").unwrap();
+        let model_computation = ModelComputation {
+            surml_file: &mut file,
+        };
 
-            let x = vec![0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
-            let data: ArrayD<f32> = ndarray::arr1(&x).into_dyn();
-            let data: ArrayD<f32> = data.into_shape((1, 28)).unwrap().into_dyn();
-    
-            let output = model_computation.raw_compute(data, None).unwrap();
-            assert_eq!(output.len(), 1);
-            assert_eq!(output[0], 0.0);
+        let mut input_values = HashMap::new();
+        input_values.insert(String::from("squarefoot"), 1000.0);
+        input_values.insert(String::from("num_floors"), 2.0);
+
+        let raw_input = model_computation.input_tensor_from_key_bindings(input_values);
+
+        let output = model_computation.raw_compute(raw_input, None).unwrap();
+        assert_eq!(output.len(), 1);
+    }
+
+    #[test]
+    fn test_buffered_compute_linear_torch() {
+        let mut file = SurMlFile::from_file("./model_stash/torch/surml/linear.surml").unwrap();
+        let model_computation = ModelComputation {
+            surml_file: &mut file,
+        };
+
+        let mut input_values = HashMap::new();
+        input_values.insert(String::from("squarefoot"), 1000.0);
+        input_values.insert(String::from("num_floors"), 2.0);
+
+        let output = model_computation.buffered_compute(&mut input_values).unwrap();
+        assert_eq!(output.len(), 1);
     }
 }
