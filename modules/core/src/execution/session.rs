e1eb1094762c72d62028332e0966e96dc5d9d882
maxwellflitton <maxwellflitton@gmail.com>
2024-12-24 02:32:56 +0000

    upgrading to beta V 2 of ort with optional compilation of GPU

--- modules/core/src/execution/session.rs
+++ modules/core/src/execution/session.rs
@@ -1,8 +1,16 @@
+//! Defines the session module for the execution module.
 use ort::session::Session;
 use crate::errors::error::{SurrealError, SurrealErrorStatus};
 use crate::safe_eject;
 
 
+/// Creates a session for a model.
+/// 
+/// # Arguments
+/// * `model_bytes` - The model bytes (usually extracted fromt the surml file)
+/// 
+/// # Returns
+/// A session object.
 pub fn get_session(model_bytes: Vec<u8>) -> Result<Session, SurrealError> {
     let builder = safe_eject!(Session::builder(), SurrealErrorStatus::Unknown);
 
